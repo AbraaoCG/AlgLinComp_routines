e0112bd561f4572f825543ff3a482b7d6580f067
AbraaoCG_Note <abraao.cg@poli.ufrj.br>
2023-06-13 13:03:22 -0300

    Método de Newton implementado

--- Non_Lin_Solvers/rootSolver.py
+++ Non_Lin_Solvers/rootSolver.py
@@ -1,26 +1,41 @@
 # Esse programa pergunta ao usuário qual o método desejado para encontrar
-# a raiz de uma função.
+# a raiz de uma função e realiza o cálculo da raiz, imprimindo resultado na tela.
 
 import numpy as np
 
-# Definir f(x)
+# Definir funções complementares.
 
+# Cálculo de X
 def func(x):
     g = 9.806
     k = 0.00341
-    f = np.log(np.cosh(x*np.sqrt(g*k)))- 50
-    f2 = x
-    return f
+    f1 = np.log10(np.cosh(x*np.sqrt(g*k)))- 50 # Método bissecante: a,b = -600,-650 ; Método de Newton x0 = 
+    f2 = ( ( 4 * np.cos(x) ) - ( np.e**(2*x) ) )
+    return f1
 
+# Cálculo da derivada primeira de x de forma literal ( com expressão dada ).
+def d1_func(x):
+    g = 9.806
+    k = 0.00341
+    # Derivada de f1 e f2 para método de Newton Original
+    df1 = (np.tanh(x*np.sqrt(g*k)) * np.sqrt(g*k)) / np.log(10)
+    df2 = ( -4 * np.sin(x) )- ((np.e**(2*x)) * 2)
+
+    return df1
+
+# Cálculo da derivada primeira de x de forma numérica ( com diferenças finitas ).
+
+def d1_func_numericMethod(xi,xa):
+    h = xi - xa
+    return ((func(xi) - func(xa)) / h)
+
+# ------------------------------------------------------------------------------------------------------
 # Definir algorítimo método da bisseção
-def bissec_method():
-    # Definir Tolerância
+def bissecMethod():
+    # Definir Tolerância e número máximo de iterações
     tol = 1e-5
+    itMax = 10000
     print(f'Tolerância utilizada: {tol}\n Para mudar, realize alteração no código.')
-    # # Definir a e b de inicio. Supoe-se que f(a) < 0 e f(b) > 0 , e que f(x) tem ao menos raiz.
-    # a = -1
-    # b = 1
-    # Perguntar Parâmetros ao usuário
     a = b = ''
     while( (type(a) != type(1.0)) and ( type(a) != type(1.0))):# Enquanto a e b não forem numéricos.
         a = float(input('Insira a: Primeiro valor de x, tal que f(a) < 0\n a = '))
@@ -31,6 +46,9 @@ def bissec_method():
     xi = np.nan ; fi = np.nan
     it = 0
     while(abs(a - b) > tol):
+        if (it > itMax):
+            print('Convergência não alcançada')
+            return np.nan    
         xi = (a+b) / 2
         fi = func(xi)
         it += 1
@@ -43,6 +61,45 @@ def bissec_method():
     print(f'Convergência alcançada em {it} iterações.')
     return xi    
 
+# ------------------------------------------------------------------------------------------------------
+# Definir Algorítimo do método de Newton
+
+def newtonMethod(method):
+    # Valores de dx, número máximo de iterações e tolerância definidos em código.
+    dx = 0.001
+    itMax = 10000
+    tol = 1e-5
+    print(f'Método de Newton utilizando: \ndx = {dx}\nNúmero máximo de iterações = {itMax}\n')
+    print("Tenha certeza de definir f(x) em código antes de executar o programa.\n Caso Método Original seja selecionado, também definda f'(x) em código.")
+    
+    # xo é definido pelo usuário, e é equivalente à xa na primeria iteração.
+    xa = float(input('Insira x0( o valor inicial de x ) para execução do método: \n xo = '))
+
+    # Algorítimo de aproximação da raiz de f(x)
+    xi = xa + dx # x na iteração atual.
+    it = 0
+    while ( it < itMax ):
+        # Cálculo de f'(x), em função do método ( Original ou Secante ).
+        if (method == 0): # Original
+            df_xi = d1_func(xi)
+        elif(method == 1): # Secante
+            df_xi = d1_func_numericMethod(xi = xi, xa = xa)
+        
+        # Cálculo do x próximo
+        x_next = xi - (func(xi) / df_xi)
+
+        if(abs(x_next - xi) <= tol):
+            print(f'O método de Newton convergiu em {it} iterações!')
+            return x_next # x_next é a raiz da função.
+        else:
+            xa = xi
+            xi = x_next
+        it += 1
+
+    # Se não retornar é porque não convergiu
+    print('O método de Newton não convergiu!')
+
+# ------------------------------------------------------------------------------------------------------
 # Apresentação do programa
 print("Esse programa é destinado ao cálculo de raizes de funções utilizando três diferentes métodos")
 print("Tenha certeza de programar sua função corretamente dentro do código.\n")
@@ -52,11 +109,13 @@ ICOD = -1
 ICOD = int(input('Qual método desejado para encontrar a raiz?\n   ICOD 1: Método da Bisseção\n   ICOD 2: Método de Newton\n   ICOD 3: Método da Interpolação inversa.\n'))
 
 match ICOD:
-    case 1:
-        root = bissec_method()
+    case 1: # Método da Bisseção.
+        root = bissecMethod()
         print(f'A raiz da função é {root}')
-    case 2:
-        pass
+    case 2: # Método de Newton.
+        method = int(input('Insira 0 para método de newton original e 1 para método de newton Secante:\n'))
+        root = newtonMethod(method)
+        print(f'A raiz da função é {root}')
     case 3:
         pass
 
--- Non_Lin_Solvers/test.py
+++ Non_Lin_Solvers/test.py
@@ -2,8 +2,8 @@ import numpy as np
 def func(x):
     g = 9.806
     k = 0.00341
-    f = np.log(np.cosh(x*np.sqrt(g*k)))- 50
+    f = np.log10(np.cosh(x*np.sqrt(g*k)))- 50
     f2 = x
     return f
 
-print(func(277.2210016846657))
+print(func(-633.387416601181))
