9ed748122c7476e151d0f4924ee54dbd71cb1fdd
AbraaoCG_Note <abraao.cg@poli.ufrj.br>
2023-06-20 17:04:27 -0300

    Finalizando interface task 1 e 2

--- Non_Lin_Solvers/nlSolver.py
+++ Non_Lin_Solvers/nlSolver.py
@@ -2,23 +2,21 @@
 import numpy as np
 # Método para obter funcoes de um arquivo de texto no formato do arquivo 'funcoes.txt'
 def getFunctions(path):
-    import math
-
     with open(path, 'r') as arquivo:
         linhas = arquivo.readlines()
-
     for linha in linhas:
         nome, expressao = linha.strip().split('=')
-
     funcoes = []
     numArgsF = []
     for linha in linhas:
         nome, expressao = linha.strip().split('=')
         args = nome.split('(')[1].split(')')[0].split(',')
         expressao = expressao.replace('log','np.log10')
+        expressao = expressao.replace('ln','np.log')
         expressao = expressao.replace('sen','np.sin')
         expressao = expressao.replace('cos','np.cos')
         expressao = expressao.replace('e','np.e')
+        expressao = expressao.replace('pi','np.pi')
         # print('lambda ' + ','.join(args) + ':' + expressao)
         funcao = eval('lambda ' + ','.join(args) + ':' + expressao)
         funcoes.append(funcao)
@@ -37,12 +35,10 @@ def partialDer(f,h,xArray,n):
     return ((fi - fa) / h)
 
 # Montar Jacobiana
-
 def getJacobianM(functionsVector,numArgsMax,xArray,hList):
     m = len(functionsVector)
     n = numArgsMax
     jacobianM = np.ndarray(shape=(m,n))
-
     for i in range(m):
         for j in range(n): # Itero sobre cada variável
             f = functionsVector[i]
@@ -50,19 +46,17 @@ def getJacobianM(functionsVector,numArgsMax,xArray,hList):
     return jacobianM
 
 # Método de Newton para resolver sistema de equações não lineares.
-
-def newtonMethod(funcPath):
+def newtonMethod(funcPath,tol):
     # Argumentos de execução
-    tol = 1e-05
     itMax = 10000
     # Obter funcoes do usuário a partir de um TXT em 'funcPath'
     functionsVector,numArgsMax = getFunctions(funcPath)
     numFunc = len(functionsVector)
     # Prepara Valores de partida
-    hList = [0.001 ] * numArgsMax
+    dxi = 0.001
+    hList = [dxi] * numArgsMax
     xArray = np.ones(numArgsMax) # X0 = Vetor coluna preenchido com '1's
     jacobianMatrix = getJacobianM(functionsVector,numArgsMax,xArray,hList) # Jacobiana Inicial
-    
     fX_Vector = np.ndarray(shape=(numFunc,1))
     it = 0
     # Algorítimo principal
@@ -84,36 +78,36 @@ def newtonMethod(funcPath):
             normDx += dX_Vector[i]**2
         normX = np.sqrt(normX) ; normDx = np.sqrt(normDx)
         tolK = normDx / normX
+        print(f'Solução na iteração {it} : {xArray}')
+        print(f'Tolerância na iteração {it} : {tolK}\n')
         # Verificar condição de parada.
         if (tolK < tol):
-            print('Convergência alcançada!')
+            print(f'Convergência alcançada na iteração {it}!')
             return xArray
         # Recalcular Jacobiana
         jacobianMatrix = getJacobianM(functionsVector,numArgsMax,xArray,dX_Vector)
-        
-    print('Convergência não alcançada!')
+        it += 1
+
+    print('\nConvergência não alcançada!')
+    return np.nan
 
 # Método de Broyden para resolver sistema de equações não lineares.
-
-def broydenMethod(funcPath):
+def broydenMethod(funcPath,tol):
     # Argumentos de execução
-    tol = 1e-05
     itMax = 10000
     # Obter funcoes do usuário a partir de um TXT em 'funcPath'
     functionsVector,numArgsMax = getFunctions(funcPath)
     numFunc = len(functionsVector)
     # Prepara Valores de partida
-    Y = [ 1 ] * numFunc
-    hList = [0.001 ] * numArgsMax
+    dxi = 0.001
+    hList = [dxi ] * numArgsMax
     xArray = np.ones(numArgsMax) # X0 = Vetor coluna preenchido com '1's
     jacobianMatrix = getJacobianM(functionsVector,numArgsMax,xArray,hList) # Jacobiana Inicial
-
     fX_Vector = np.ndarray(shape=(numFunc,1))
     it = 0
     # Algorítimo principal
     while(it < itMax):
         # Computar inversa da Jacobiana
-        # print(jacobianMatrix)
         invJacM = np.linalg.inv(jacobianMatrix)
         # Computar f(Xk)
         for i in range(numFunc):
@@ -130,34 +124,38 @@ def broydenMethod(funcPath):
             normDx += dX_Vector[i]**2
         normX = np.sqrt(normX) ; normDx = np.sqrt(normDx)
         tolK = normDx / normX
+        print(f'Solução na iteração {it} : {xArray}')
+        print(f'Tolerância na iteração {it} : {tolK}\n')
         # Verificar condição de parada.
         if (tolK < tol):
-            print('Convergência alcançada!')
+            print(f'Convergência alcançada na iteração {it}!')
             return xArray
+        it = it + 1
         # Recalcular Jacobiana com método de Broyden
         Yk = np.ndarray(shape=(numFunc,1))
         for i in range(numFunc): # Computar Y a partir de f(Xk+1) - f(Xk)
             Yk[i][0] = functionsVector[i](*xArray) - fX_Vector[i][0] 
         dX_MatrixT = dX_Matrix.T
         jacobianMatrix += ( np.dot(Yk - ( np.dot(jacobianMatrix,dX_Matrix) ) , dX_MatrixT)  / np.dot(dX_MatrixT,dX_Matrix))
-        
-    print('Convergência não alcançada!')
+    print('\nConvergência não alcançada!')
+    return np.nan
 
 
+
+print("Insira no arquivo funcoes.txt as funções fi(x) tal que fi(x) = 0\n Faça isso no formato f(x1,...,xn) = ... , onde n é o número máximo de variáveis no sistema de equações.\n")
 funcPath = 'funcoes.txt'
-xArr = newtonMethod(funcPath)
-print(xArr)
+tol = float(input("Insira a tolerância aceita: "))
+ICOD = int(input("Insira o método de solução: \n Método de Newton -> 1\n Método de Broyden -> 2\nICOD : "))
 
-functionsVector,numArgsMax = getFunctions(funcPath)
-for f in functionsVector:
-    print(f(*xArr))
+xArr = np.nan
+if (ICOD == 1):
+    xArr = newtonMethod(funcPath=funcPath,tol=tol)
 
-# print(getJacobianM(functionsVector,numArgsMax,[1,1,1],[0.001,0.001,0.001]))
+elif(ICOD == 2):
+    xArr = broydenMethod(funcPath=funcPath,tol=tol)
 
-# ---------------------------------------
 
-# xArray = [2,1,1]
-# functionsVector,numArgsMatrix = getFunctions('funcoes.txt')
-# jacobianMatrix = getJacobianM(functionsVector,numArgsMatrix,xArray)
-
-# print(jacobianMatrix)
+if(type(xArr) != type(np.nan)):
+    print(f'\nO Vetor solução é{xArr}')
+else:
+    print('Não foi encontrado resultado válido.\n Caso tenha convergido, revise a entrada das funções e os argumentos e tente novamente')
