d0179315359590a6243e7c173e0a0ef408624f92
abraao-PC-Win <abraao.cg@poli.ufrj.br>
2023-06-20 20:41:11 -0300

    Int. auto adaptativa implementada

--- Numeric_Intg/numIntg.py
+++ Numeric_Intg/numIntg.py
@@ -40,7 +40,6 @@ def construct_k_vector(a, b, n):
     K = np.ndarray(shape=(n,1))
     for i in range(1,n+1):
         K[i-1][0] = (b**i - a**i) / i
-    # K[0] = 1.0  # Tratamento especial para k = 0 para evitar divisão por zero
     return K
 
 def getW(xArr, a, b):
@@ -48,9 +47,6 @@ def getW(xArr, a, b):
     VandM_Matrix = construct_vandermonde_matrix(xArr)
     KMatrix = construct_k_vector(a, b, n)
 
-    for i in range(len(VandM_Matrix)):
-        print(VandM_Matrix[i][:])
-    print(f'\n KMat == \n{KMatrix}')
     W = np.linalg.solve(VandM_Matrix, KMatrix)
     return W
 
@@ -81,21 +77,32 @@ def getFunctions(path):
 
 
 def Poly_Integ_adaptative(f, a, b, tol):
-
-    return integrate_polynomial(f, a, b, 4)
+    it = 2 ; itMax = 10000
+    Ik = integrate_polynomial(f=f, a=a, b=b, n=1 ) # Calcula integral com 1 ponto.
+    while(it < itMax):
+        Ik_proximo = integrate_polynomial(f=f, a=a, b=b, n=it) # Calcula integral 
+        tolk = abs(Ik_proximo - Ik) / abs(Ik_proximo)
+        print(f'TolK com {it} pontos = {tolk}')
+        Ik = Ik_proximo
+        if (tolk < tol):
+            print(f'Tolerância desejada alcançada com n = {it}')
+            return Ik_proximo
+        it+= 1
+    print(f'Não houve convergência para a tolerância desejada em {itMax} iterações.')
 
 def QuadGauss_Integ_adaptative(f, a, b, tol):
-    
-    return integrate_gauss(f, a, b, 3, gauss_weights)
-
-# # Exemplo de uso da função de integração polinomial
-# integral_polynomial = integrate_polynomial(f, 0, 5, 10)
-# print("Integração Polinomial:", integral_polynomial)
-
-# # Exemplo de uso da função de integração por quadratura de Gauss
-# integral_gauss = integrate_gauss(f, 0, 5, 10, gauss_weights)
-# print("Integração Gauss:", integral_gauss)
-
+    it = 2 ; itMax = 10000
+    Ik = integrate_gauss(f=f, a=a, b=b, n=1, weight_func=gauss_weights)  # Calcula integral com 1 ponto.
+    while(it < itMax):
+        Ik_proximo = integrate_gauss(f=f, a=a, b=b, n=it, weight_func=gauss_weights)  
+        tolk = abs(Ik_proximo - Ik) / abs(Ik_proximo)
+        print(f'TolK com {it} pontos = {tolk}')
+        Ik = Ik_proximo
+        if (tolk < tol):
+            print(f'Tolerância desejada alcançada com n = {it}')
+            return Ik_proximo
+        it+= 1
+    print(f'Não houve convergência para a tolerância desejada em {itMax} iterações.')
 
 ICOD = int(input("Programa de Integração numérica. Defina a função no arquivo 'funcaoInt.txt' e selecione o tipo de integração desejado:\n 1 : Integração Polinomial\n 2 : Integração por Quadratura de Gauss\n Seleção = "))
 
@@ -103,7 +110,7 @@ funcs,numF = getFunctions('funcaoInt.txt')
 
 if(numF > 0):
     f = funcs[0]
-    a = float(input("Seja a e b os limites de integração: a --> b\n Insira a: "))
+    a = float(input("Seja a e b os limites de integração: a --> b\nInsira a: "))
     b = float(input("Insira b: "))
     tol = float(input("Insira a tolerância aceita: "))
 
